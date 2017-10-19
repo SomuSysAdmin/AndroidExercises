75dd9816f90bd118f7fa40f8c15345ca7a9af197
Somu <somu.sysadm@gmail.com>
2017-10-19 11:33:29 +0530

    Initial commit - added base files

---
+++ WebDownload/app/src/main/java/com/example/somu/webdownload/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.somu.webdownload;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
