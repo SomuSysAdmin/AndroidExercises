3cc71206a4c308b022e16f4d021ecd2caf7d3a89
Somu <somu.sysadm@gmail.com>
2017-10-19 13:47:06 +0530

    Version 1 where the filename is printed to the console.

--- WebDownload/app/src/main/java/com/example/somu/webdownload/MainActivity.java
+++ WebDownload/app/src/main/java/com/example/somu/webdownload/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.somu.webdownload;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            return null;
+
+            Log.i("URL", params[0]);
+            return "Done";
         }
     }
 
@@ -18,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask dl = new DownloadTask();
+        String result = null;
+        try {
+            result = dl.execute("https://www.SomuSysAdmin.com/").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        Log.i("Result", result);
     }
 }
