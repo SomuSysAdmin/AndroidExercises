53153efc78f2279b56e480a9b9ed7f00597a68b3
Somu <somu.sysadm@gmail.com>
2017-10-21 23:15:02 +0530

    Added the AsyncTask derived class

--- 078DownImage/app/src/main/java/com/example/somu/a078downimage/MainActivity.java
+++ 078DownImage/app/src/main/java/com/example/somu/a078downimage/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.somu.a078downimage;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class ImageDownload extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
