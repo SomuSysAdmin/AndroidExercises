19e804166223a8275a1ac7adaefedeecb481118a
Somu <somu.sysadm@gmail.com>
2017-10-22 10:12:44 +0530

    Completed coding, deleted the .git folder for unused root.

--- 078DownImage/app/src/main/java/com/example/somu/a078downimage/MainActivity.java
+++ 078DownImage/app/src/main/java/com/example/somu/a078downimage/MainActivity.java
@@ -8,13 +8,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView downloadedImg;
 
     public class ImageDownload extends AsyncTask<String, Void, Bitmap>
     {
@@ -22,21 +23,31 @@ public class MainActivity extends AppCompatActivity {
         protected Bitmap doInBackground(String... urls) {
             try {
                 URL imgSrc = new URL(urls[0]);
-                HttpsURLConnection httpsCon = (HttpsURLConnection) imgSrc.openConnection();
+                HttpURLConnection httpsCon = (HttpURLConnection) imgSrc.openConnection();
                 httpsCon.connect();
 
                 InputStream inputStream = httpsCon.getInputStream();
                 Bitmap img = BitmapFactory.decodeStream(inputStream);
 
+                return img;
 
             } catch (Exception e) {
                 e.printStackTrace();
+                return null;
             }
         }
     }
 
     void downloadImage(View view) {
         // http://www.freepngimg.com/thumb/internet_meme/4-2-yao-ming-meme-png-thumb.png
+
+        ImageDownload task = new ImageDownload();
+        try {
+            Bitmap myImg = task.execute("http://www.freepngimg.com/thumb/internet_meme/4-2-yao-ming-meme-png-thumb.png").get();
+            downloadedImg.setImageBitmap(myImg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Log.i("Test", "Boilerplate Junk!");
     }
@@ -46,6 +57,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView downloadedImg = (ImageView) findViewById(R.id.imageView);
+        downloadedImg = (ImageView) findViewById(R.id.imageView);
     }
 }
